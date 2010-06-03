12fb1d9cad824c754c4c24747f23428c7bcb1cba
thorncp <thorncp@gmail.com>
2010-06-02 22:53:09 -0800

    Approach 3, a rails-ish solution.  My favorite so far.

---
+++ mockups/approach3.rb
@@ -0,0 +1,37 @@
+# rails-ish approach
+
+## resources/greeting.rb
+class Greeting < Rubot::Core::Resource
+  attr_accessor :val
+end
+
+## controllers/greeting_controller.rb
+class GreetingController < Rubot::Core::Controller
+  # !greet Steve_ => "Hello there Steve_"
+  command :greet do |options|
+    # server and message are method calls that will return the appropriate objects
+    server.msg(message.destination, Greeting.val % message.body)
+  end
+  
+  # Steve_ joins #resdat => "Hello there Steve_"
+  on :join do
+    server.msg(message.destination, Greeting.val % message.from)
+  end
+  
+  listen do
+    # listen to all messages
+  end
+  
+  listen :from => "Steve_" do
+    # listen for all messages from Steve_
+  end
+  
+  listen :matches => /some_regex/ do
+    # listen for a message that matches a pattern
+  end
+  
+  # initialize the resource when necessary
+  on :startup, :reload do
+    Greeting.val = "Hello there %s"
+  end
+end
