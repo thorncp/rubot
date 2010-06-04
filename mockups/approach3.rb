0e0d30571b6317e46d47e290a38f1fa68e492599
thorncp <thorncp@gmail.com>
2010-06-03 20:27:39 -0800

    updated the likeliest of approaches with a few more examples. testing the mockup waters of what's doable

--- mockups/approach3.rb
+++ mockups/approach3.rb
@@ -1,18 +1,45 @@
-# rails-ish approach
-
 ## resources/greeting.rb
-class Greeting < Rubot::Core::Resource
-  class << self
-    attr_accessor :val
+
+# a few types of resources depending on what you want to do
+
+# data kept in memory
+class Greeting < Rubot::Resource::Static
+  # basically creates attributes on the class
+  resource :val
+end
+
+# data kept in db
+class Greeting < Rubot::Resource::Model
+  # ORM stuff here, or some other config location TBD
+end
+
+# web resource
+class Greeting < Rubot::Resource::Web
+  # maybe differentiate between screen scraping and web apis with different classes TBD
+  resource "http://some-web-resource.com/", :as => :val, :params => [:param_one, :param_two] do |response|
+    # handle response
+    response[/val=(.+)/i, 1]
   end
+  
+  # to be invoked like
+  #   val = Greeting.val(:param_one => "some value", :param_two => "another value")
+  #
+  # or, if ruby >= 1.9, take advantage of ordered hashes
+  #   val = Greeting.val("some value", "another value") # note: this could possibly get quite ugly
 end
 
 ## controllers/greeting_controller.rb
 class GreetingController < Rubot::Core::Controller
   # !greet Steve_ => "Hello there Steve_"
-  command :greet do |options|
+  command :greet do
     # server and message are method calls that will return the appropriate objects
     server.msg(message.destination, Greeting.val % message.body)
+  end
+  
+  command :hi, :hello do
+    # options is a method call that will return the appropriate object
+    reply = options[:alias] == :hi ? "hi there %s" : "hello there %s"
+    server.msg(message.destination, reply % message.from)
   end
   
   # Steve_ joins #resdat => "Hello there Steve_"
