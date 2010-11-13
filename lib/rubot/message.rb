ec7440c79e598b73e33cc205a8c7454e4334461b
thorncp <thorncp@gmail.com>
2010-11-13 12:33:09 -0900

    change access level of message attributes

--- lib/rubot/message.rb
+++ lib/rubot/message.rb
@@ -1,9 +1,9 @@
 module Rubot
   class Message
-    attr_accessor :from, :to, :text
+    attr_reader :from, :to, :text
     
     def initialize(args = {})
-      args.each { |k,v| send("#{k}=", v) }
+      args.each { |k,v| instance_variable_set("@#{k}", v) }
     end
   end
 end
