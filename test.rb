829967d3ba7182c8651fc0250b0ba37b922fa31d
thorncp <thorncp@gmail.com>
2010-12-02 20:27:06 -0900

    flesh out the test file a bit

--- test.rb
+++ test.rb
@@ -2,6 +2,23 @@ $:.unshift "./lib/"
 require "rubot"
 require "eventmachine"
 
+class SupController < Rubot::Controller
+  command :yo do
+    server.message "#rubot", "what up cuz"
+  end
+  
+  command :echo do
+    unless message.text.empty?
+      server.message message.to, message.text
+    else
+      server.message message.to, "no text"
+    end
+  end
+end
+
+dispatcher = Rubot::Dispatcher.new
+dispatcher.instance_variable_set :@controllers, [SupController]
+
 EventMachine::run do
-  EventMachine::connect 'irc.freenode.net', 6667, Rubot::Server, Rubot::Dispatcher.new, :nick => "rubot-edge", :verbose => true, :channels => ["#rubot"]
+  EventMachine::connect 'irc.freenode.net', 6667, Rubot::Server, dispatcher, :nick => "rubot-edge", :verbose => true, :channels => ["#rubot"]
 end
