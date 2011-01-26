3659cd44abda49ffb6b5ef845d4e5c40bafbef39
thorncp <thorncp@gmail.com>
2011-01-25 17:08:11 -0900

    rename variable

--- spec/rubot/commands_spec.rb
+++ spec/rubot/commands_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 module Rubot
   describe Commands do
     before :each do
-      @controller = Class.new do
+      @commander = Class.new do
         extend Commands
         command(:yo_dawg) { "sup" }
       end
@@ -11,34 +11,34 @@ module Rubot
     
     describe ".execute?" do
       it "should claim to be able to execute a defined command" do
-        @controller.should satisfy { |c| c.execute?("yo_dawg") }
+        @commander.should satisfy { |c| c.execute?("yo_dawg") }
       end
       
       it "should not clame to execute an undefined command" do
-        @controller.should_not satisfy { |c| c.execute?("invalid") }
+        @commander.should_not satisfy { |c| c.execute?("invalid") }
       end
       
       it "should compare the string representation of the given param" do
-        @controller.should satisfy { |c| c.execute?(:yo_dawg) }
+        @commander.should satisfy { |c| c.execute?(:yo_dawg) }
       end
     end
     
     describe ".execute" do
       it "should execute the given command" do
-        @controller.execute(:yo_dawg).should == "sup"
+        @commander.execute(:yo_dawg).should == "sup"
       end
       
       it "should raise when attempting to execute an undefined command" do
-        lambda { @controller.execute :invalid }.should raise_error(NoCommandError)
+        lambda { @commander.execute :invalid }.should raise_error(NoCommandError)
       end
       
       it "should execute in the scope of an instance" do
-        @controller.class_exec do
+        @commander.class_exec do
           command(:test_instance_mehod) { my_instance_method }
           define_method(:my_instance_method) {}
         end
         
-        lambda { @controller.execute :test_instance_mehod }.should_not raise_error
+        lambda { @commander.execute :test_instance_mehod }.should_not raise_error
       end
     end
   end
