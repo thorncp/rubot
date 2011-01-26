9a6011d352947050947d0b02097ac1d17d94e203
thorncp <thorncp@gmail.com>
2011-01-25 20:45:25 -0900

    update listener specs
    the specs were expecting a call to :listen to return a value. this isn't reasonable as calling listen could potentially trigger several defined listeners. for now, i think this new hack does what it's supposed to, but it sure is ugly

--- spec/rubot/listeners_spec.rb
+++ spec/rubot/listeners_spec.rb
@@ -1,17 +1,24 @@
 require "spec_helper"
+require "ostruct"
 
 module Rubot
   describe Listeners do
+    before :each do
+      # such a hack. todo: refactor please
+      @@derp = nil
+    end
+    
     describe "no restrictions" do
       before :each do
         @listener = Class.new do
           extend Listeners
-          listen { 42 }
+          listener { @@derp = 42 }
         end
       end
       
       it "should listen to any message" do
-        @listener.listen(Message.new(text: "a witty statement")).should eql(42)
+        @listener.listen(Message.new(text: "a witty statement"))
+        @@derp.should eql(42)
       end
     end
     
@@ -19,21 +26,24 @@ module Rubot
       before :each do
         @listener = Class.new do
           extend Listeners
-          listen(:match => /o snap/) { 42 }
-          listen(:match => /what up (\w+)/) { matches[1] }
+          listen(:match => /o snap/) { @@derp = 42 }
+          listen(:match => /what up (\w+)/) { @@derp = matches[1] }
         end
       end
       
       it "should listen to matched message" do
-        @listener.listen(Message.new(text: "omg o snap irl")).should eql(42)
+        @listener.listen(Message.new(text: "omg o snap irl"))
+        @@derp.should eql(42)
       end
       
       it "should not listen to unmatched message" do
-        @listener.listen(Message.new(text: "snap crackle pop")).should_not eql(42)
+        @listener.listen(Message.new(text: "snap crackle pop"))
+        @@derp.should_not eql(42)
       end
       
       it "should pass along capture groups" do
-        @listener.listen(Message.new(text: "what up cuz")).should eql("cuz")
+        @listener.listen(Message.new(text: "what up cuz"))
+        @@derp.should eql("cuz")
       end
     end
     
@@ -41,16 +51,18 @@ module Rubot
       before :each do
         @listener = Class.new do
           extend Listeners
-          listen(:from => "thorncp") { 42 }
+          listen(:from => "thorncp") { @@derp = 42 }
         end
       end
       
       it "should listen to message from specified nick" do
-        @listener.listen(Message.new(from: "thorncp")).should eql(42)
+        @listener.listen(Message.new(from: "thorncp"))
+        @@derp.should eql(42)
       end
       
       it "should not listen to message from unspecified nick" do
-        @listener.listen(Message.new(from: "trollface")).should_not eql(42)
+        @listener.listen(Message.new(from: "trollface"))
+        @@derp.should_not eql(42)
       end
     end
     
@@ -58,16 +70,18 @@ module Rubot
       before :each do
         @listener = Class.new do
           extend Listeners
-          listen(:to => "#rubot") { 42 }
+          listen(:to => "#rubot") { @@derp = 42 }
         end
       end
       
       it "should listen to message to specified location" do
-        @listener.listen(Message.new(to: "#rubot")).should eql(42)
+        @listener.listen(Message.new(to: "#rubot"))
+        @@derp.should eql(42)
       end
       
       it "should not listen to message to unspecified location" do
-        @listener.listen(Message.new(to: "#ruby")).should_not eql(42)
+        @listener.listen(Message.new(to: "#ruby"))
+        @@derp.should_not eql(42)
       end
     end
   end
