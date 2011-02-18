087e1ddeac6aaab24cb0d92ac8843e3dadcde869
thorncp <thorncp@gmail.com>
2011-02-17 17:38:17 -0900

    execute commands and listeners in threads

--- lib/rubot/dispatcher.rb
+++ lib/rubot/dispatcher.rb
@@ -9,35 +9,41 @@ module Rubot
         @controllers << klass
       end
     end
-    
+
     def message_received(server, message)
-      # todo: consider spamming all controllers with the command, and let them decide
-      # for themselves whether or not to execute it. this will be how listeners will work,
-      # and will make things more consistent
-      #
-      # hmmmm, maybe the listeners will behave like the commands currently do...
-      #
-      # no, it doesn't make sense for a listener to check to see if it should listen, return true,
-      # then check AGAIN to find the proc to execute.
-      
       args = {
         :server => server,
         :dispatcher => self,
         :message => message
       }
-      
+
       if controller = find_contoller(message)
-        controller.execute(message.alias, args)
+        wrap { controller.execute(message.alias, args) }
       else
-        @controllers.each { |c| c.listen(message, args) }
+        @controllers.each do |c|
+          wrap { c.listen(message, args) }
+        end
       end
     end
-    
+
     def find_contoller(message)
+      # pull function character to config
       if match = message.text.match(/^!(\w+)( .*)?$/i)
         message.alias = match[1]
         message.text.sub!("!#{match[1]}", "").strip
         @controllers.find { |c| c.execute?(match[1]) }
+      end
+    end
+
+    def wrap(&block)
+      # take into account the config value of :verbose,
+      # that involves getting the config into this class
+      Thread.new do
+        begin
+          block.call
+        rescue RuntimeError => e
+          puts e.inspect
+        end
       end
     end
   end
