f1fd9e02ab0927633a748e12955210827c375afb
thorncp <thorncp@gmail.com>
2011-03-02 21:59:58 -0900

    add params to web resource

--- lib/rubot/web_resource.rb
+++ lib/rubot/web_resource.rb
@@ -1,13 +1,20 @@
 require "net/http"
 require "nokogiri"
+require "cgi"
 
 module Rubot
   class WebResource
     def self.get(name, url)
-      define_singleton_method(name) do
-        response = Net::HTTP.get(URI.parse(url))
-        yield Nokogiri::HTML.parse(response)
+      define_singleton_method(name) do |params = {}|
+        yield Nokogiri::HTML.parse(get_wut(url, params))
       end
+    end
+
+    private
+    # todo: name
+    def self.get_wut(url, params = {})
+      query = "?" + params.map { |k,v| "#{k}=#{CGI.escape(v)}" }.join("&") if params.any?
+      Net::HTTP.get(URI.parse(url + query.to_s))
     end
   end
 end
