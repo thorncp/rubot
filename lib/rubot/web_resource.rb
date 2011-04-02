6f882a44056310ebfc690f3408aae41136813b48
Chris Thorn <thorncp@gmail.com>
2011-04-02 15:39:58 -0800

    tiny refactor of web resource

--- lib/rubot/web_resource.rb
+++ lib/rubot/web_resource.rb
@@ -6,15 +6,13 @@ module Rubot
   class WebResource
     def self.get(name, url)
       define_singleton_method(name) do |params = {}|
-        yield Nokogiri::HTML.parse(get_wut(url, params))
+        yield get_url(url, params)
       end
     end
 
-    private
-    # todo: name
-    def self.get_wut(url, params = {})
+    def self.get_url(url, params = {})
       query = "?" + params.map { |k,v| "#{k}=#{CGI.escape(v)}" }.join("&") if params.any?
-      Net::HTTP.get(URI.parse(url + query.to_s))
+      Nokogiri::HTML.parse Net::HTTP.get(URI.parse(url + query.to_s))
     end
   end
 end
